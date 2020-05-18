166c607bb25b7ec56bf0910784efd4f8f8d60aa9
danny099 <dalmariv01@gmail.com>
2020-05-18 05:32:34 -0500

    probar en contenedor

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,3 +21,12 @@ function shorten (text, maxLength) {
   }
   return ret;
 }
+
+const url = 'http://127.0.0.1:5001/api/v1/status/';
+$.get(url, function (data, status) {
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
+});
